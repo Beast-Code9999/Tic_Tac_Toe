970bcf2472b275e70a7e6d20ea1c1995420f83a5
Beast Code <beastcode9999@gmail.com>
2022-10-30 21:10:07 +0900

    add commented logic of code

--- app.py
+++ app.py
@@ -1,2 +1,18 @@
 # welcome
-print('hello world')
+
+print('Welcome to Tic Tac Toe!')
+
+#while True:
+    # Set the game up here
+    #pass
+
+    #while game_on:
+        #Player 1 Turn
+        
+        
+        # Player2's turn.
+            
+            #pass
+
+    #if not replay():
+        #break
