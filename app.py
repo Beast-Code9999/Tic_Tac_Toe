4a122269d9a9e202eff152bcecdb8f60f83e53a4
Beast Code <beastcode9999@gmail.com>
2022-10-30 21:16:29 +0900

    add display_board()

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
 # welcome
 
+from cgi import test
+
+
 print('Welcome to Tic Tac Toe!')
+
+# print out a board
+
+test_board = ['#','X','O','X','O','X','O','X','O','X']
+
+def display_board( board ):
+    print(board[7] + '|' + board[8] + '|' + board[9])
+    print(board[4] + '|' + board[5] + '|' + board[6])
+    print(board[1] + '|' + board[2] + '|' + board[3])
+
+display_board( test_board )
+
+
 
 #while True:
     # Set the game up here
